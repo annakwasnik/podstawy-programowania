c14aa2ede2052f4261fb47c7867cb8ac24af1fae
annakwasnik <85489534+annakwasnik@users.noreply.github.com>
2021-10-21 18:12:07 +0200

    Add files via upload

---
+++ zadanie4.cpp
@@ -0,0 +1,17 @@
+using namespace std;
+
+int main()
+{
+	float a, b, c, d;
+	cout << "Program obliczajacy srednia arytmetyczna 4 liczb zmiennoprzecinkowych " << endl;
+	cout << "Podaj pierwsza liczbe " << endl;
+	cin >> a ;
+	cout << "Podaj druga liczbe " << endl;
+	cin >> b;
+	cout << "Podaj trzecia liczbe " << endl;
+	cin >> c ;
+	cout << "Podaj czwarta liczbe " << endl;
+	cin >> d ;
+	cout << "Srednia arytmetyczna tych liczb to: " << (a + b + c + d) / 4;
+
+}
